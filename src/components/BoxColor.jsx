5aed0024b68ac7250868d67ce2283911d78954f8
HannaFarha <johnnyfarha1@gmail.com>
2023-12-11 20:08:17 +0100

    Done

---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,15 @@
+function BoxColor(){
+    return (
+        <div>
+    
+    <BoxColor r={255} g={0} b={0} />
+<BoxColor r={128} g={255} b={0} />
+        </div>
+    
+    
+    
+    );
+    
+    }
+    
+    export default BoxColor;
