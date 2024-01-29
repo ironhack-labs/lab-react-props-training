403cf9215f952ffcb0c189025ff02b187ac89da1
VladysslavTkach <tkachacc.program@gmail.com>
2024-01-29 16:58:20 +0100

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import BoxColor from "./Components/BoxColor";
+import Greetings from "./Components/Greetings";
+import IdCard from "./Components/IdCard";
+import Random from "./Components/Random";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> LAB | React Training</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1> LAB | React Training</h1>
+			<IdCard
+				lastName="Delores "
+				firstName="Obrien"
+				gender="female"
+				height={172}
+				birth={new Date("1988-05-11")}
+				picture="https://randomuser.me/api/portraits/women/44.jpg"
+			/>
+			<Greetings lang="de">John</Greetings>
+			<Random min={12} max={33} />
+      <BoxColor red={255} green={0} blue={0} />
+      <BoxColor red={145} green={214} blue={33} />
+		</div>
+	);
 }
 
 export default App;
