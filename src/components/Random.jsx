892ccc7e4fc65681a1765a7d55b910f4905efaee
Juan Yuste Del Valle <kurobaa@gmail.com>
2024-01-29 19:46:12 +0100

    mimor changes

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,7 +1,8 @@
 const Random = (props) => {
   const minValue = props.min;
   const maxValue = props.max;
-  const randomValue = Math.floor(Math.random() * (maxValue - minValue));
+  const randomValue =
+    Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   const text = `Random value between ${minValue} and ${maxValue} => ${randomValue}`;
   return (
     <div>
