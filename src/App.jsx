0c8297bd927cdab2b4750e0d1cff9292c51fe146
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-01-29 17:25:04 +0100

    first iteration, cardId

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
+import { IdCard } from "./components/IdCard";
+
+const names = [
+  {
+    firstName: "John",
+    lastName: 'Doe',
+    gender: 'male' ,
+    height: 178,
+    birth: "1992-07-14",
+    picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+  },
+{
+    firstName: "John",
+    lastName: 'Doe',
+    gender: 'male' ,
+    height: 178,
+    birth: "1992-07-14",
+    picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <div>
+      {
+        names.map((name) => {
+          return <IdCard {...name} />
+        })
+      }
+      </div>
     </div>
   );
 }
