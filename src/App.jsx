1e460122927d96d0afdc0bfe2abdb13d7b926ede
MateusCTO <farias.mateus@gmail.com>
2024-02-05 22:52:14 +0000

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import "./App.css";
+import IdCard from "./components/IdCard";
+import Greetings from "./components/Greetins";
+import Random from "./components/Random";
+import BoxColor from "./components/BoxColor";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <IdCard
+        card={{
+          lastName: "Doe",
+          firstName: "John",
+          gender: "Male",
+          height: 178,
+          birth: new Date("1992-07-14"),
+          picture: "https://randomuser.me/api/portraits/men/44.jpg",
+        }}
+      />
+      <IdCard
+        card={{
+          lastName: "Delores",
+          firstName: "Obrien",
+          gender: "Female",
+          height: 172,
+          birth: new Date("1988-05-11"),
+          picture: "https://randomuser.me/api/portraits/women/44.jpg",
+        }}
+      />
+
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,3 @@
+function CreditCard(props) {
+  return {};
+}
