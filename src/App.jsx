9562886e96d5df9df423c31b861824c7cef3cc4e
Manuel Tavares <manueldiogotv@gmail.com>
2024-02-05 20:06:19 +0000

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
 import "./App.css";
 
+import "./components/IdCard";
+
+import "./components/Greetings";
+
+import IdCard from "./components/IdCard";
+
+import Greetings from "./components/Greetings";
+
+import "./components/Random"
+
+import Random from "./components/Random";
+
+import "./components/BoxColor";
+
+import BoxColor from "./components/BoxColor";
+
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+
+      <IdCard person={{
+        firstName: "John",
+        lastName: 'Doe',
+        firstName: 'John',
+        gender: 'male',
+        height: 178,
+        birth: "1992-07-14",
+        picture: "https://randomuser.me/api/portraits/men/44.jpg",
+
+
+      }}
+      />
+
+      <IdCard person={{
+        lastName: 'Delores',
+        firstName: 'Obrien',
+        gender: 'female',
+        height: 172,
+        birth: "1988-05-11",
+        picture: "https://randomuser.me/api/portraits/women/44.jpg"
+      }}
+      />
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Greetings lang="en">Michael</Greetings>
+      <Greetings lang="es">Diego</Greetings>
+
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
+
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Random.jsx
@@ -0,0 +1,13 @@
+function Random (props) {
+
+    const {min, max} = props;
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min
+
+    return (
+    <p className="Random">Random value between {min} and {max} {"=>"} {randomNumber}</p>
+    )
+}
+
+export default Random
+
+    
