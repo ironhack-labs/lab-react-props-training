35e846c8bfb929eb3d873b96b3e3e6650de887dc
Matheus Santos <mathsantos52@gmail.com>
2024-02-06 23:22:28 +0000

    done

---
+++ src/components/Random.jsx
@@ -0,0 +1,12 @@
+
+const Random = (props) => {
+    let randomNumber = Math.floor(Math.random() * (props.max - props.min + 1)+ props.min);
+
+    return (
+         <p>
+        Random value between {props.min} and {props.max} {"=>"} {randomNumber}
+        </p>
+    ); 
+}
+
+export default Random;
