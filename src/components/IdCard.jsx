70034ad475b5676d51a1ee292a5fdc086ec24966
arisar86 <ari.sariannidis@gmail.com>
2024-02-06 14:15:25 +0100

    added styles (still in progress)

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -1,26 +1,27 @@
+import React from "react";
 import "./IdCard.css";
 
 function IdCard(props) {
   return (
     <div className="IdCard">
-      <p>
-        <b>Last name:</b> {props.lastName}
-      </p>
-      <p>
-        <b>First name:</b> {props.firstName}
-      </p>
-      <p>
-        <b>Gender:</b> {props.gender}
-      </p>
-      <p>
-        <b>Height:</b> {props.height}
-      </p>
-      <p>
-        <b>Birth:</b> {props.birth}
-      </p>
-      <p>
-        <img src={props.imageSrc} alt="portrait" />
-      </p>
+      <img src={props.imageSrc} alt="portrait" />
+      <div className="text">
+        <p>
+          <b>Last name:</b> {props.lastName}
+        </p>
+        <p>
+          <b>First name:</b> {props.firstName}
+        </p>
+        <p>
+          <b>Gender:</b> {props.gender}
+        </p>
+        <p>
+          <b>Height:</b> {props.height}
+        </p>
+        <p>
+          <b>Birth:</b> {props.birth}
+        </p>
+      </div>
     </div>
   );
 }
