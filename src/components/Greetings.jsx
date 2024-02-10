1dc84c42dd7fd8264cafa95a16742212c401ea11
Senay Asikoglu <senayasikoglu@gmail.com>
2024-02-10 22:37:15 +0100

    add  missing languages

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -2,6 +2,8 @@ function Greetings(props) {
   const dictionary = [
     { lang: "de", greeting: "Hallo" },
     { lang: "fr", greeting: "Bonjour" },
+    { lang: "es", greeting: "Ola" },
+    { lang: "en", greeting: "Hello" }
   ];
 
   let greetingMsg = dictionary.find((x) => x.lang === props.lang).greeting;
