3cac7e2aef38f3a3e09b00484986194f5eb46d60
meritxellavila <meriavila66@gmail.com>
2024-02-13 21:48:30 +0100

    primera parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import IdCard from "./components/IdCar";
+
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <IdCard />
     </div>
+
   );
 }
 
 export default App;
+
+
+
