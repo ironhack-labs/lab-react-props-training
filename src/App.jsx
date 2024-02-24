a28c5b1385b0e21f0476d7da79251bf768fa73de
Gavin Alexander <gavalex7@aol.com>
2024-02-24 23:39:00 +0100

    2/3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
+import Greetings from "./components/Greetings";
+import Random from "./components/Random";
+import BoxColor from "./components/BoxColor";
 
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <Greetings lang="de">Ludwig</Greetings>
+  <Greetings lang="fr">François</Greetings>
+  <Greetings lang="en">John</Greetings>
+
+  <Random min={1} max={6}/>
+<Random min={1} max={100}/>
+
+
+<BoxColor/>
     </div>
   );
 }
 
+
+
+
+
 export default App;
+
