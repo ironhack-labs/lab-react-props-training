ae310a179eef867464f0f22cbe8ba43fb3c887bd
Carolina <cisj24@gmail.com>
2024-03-06 08:35:46 +0100

    Solved Lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import "./App.css";
+import { IdCard } from "./components/IdCard";
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <h1> IdCard</h1>
+
+      <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+      <div>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
+        <Greetings lang="es">Carlos</Greetings>
+        <Greetings lang="en">Joshua</Greetings>
+      </div>
+      <div>
+        <Random min={1} max={6} />
+        <Random min={1} max={100} />
+      </div>
     </div>
   );
 }
