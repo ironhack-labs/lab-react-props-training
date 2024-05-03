bae53836b8b40c9c4f3dc51672d99876a9b0319e
Oriol Roig <o.roig.hernandez@gmail.com>
2024-05-03 14:54:37 +0200

    done

--- src/components/idCard.jsx
+++ src/components/idCard.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function IdCard(props) {
   return (
     <div id="card">
-      <img className="pic" src={props.picture}></img>
+      <img className="pic" src={props.picture} />
 
       <div id="texto">
-        <p>Last Name:{props.lastName} </p>
-        <p>First Name:{props.firstName} </p>
-        <p>Gender:{props.gender} </p>
-        <p>Height:{props.height} </p>
-        <p>Birth:{props.birth} </p>
+        <p>Last Name: {props.lastName} </p>
+        <p>First Name: {props.firstName} </p>
+        <p>Gender: {props.gender} </p>
+        <p>Height: {props.height} </p>
+        <p>Birth: {props.birth.toLocaleString()} </p>
       </div>
     </div>
   );
