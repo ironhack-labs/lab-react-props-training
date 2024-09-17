aed00bda3b8cc51631293f9a871e9f20bdcbd03c
Gemma Fernandez <gemmazuera@gmail.com>
2024-09-17 15:33:43 +0200

    1 y 2 acabados

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+function IdCard(props) {
+  
+ 
+    return (
+    <div className="id-card">
+        <img src={props.picture} style={{width: "190px", height: "190px"}}/>
+        <div className="infoCard"> 
+            <p>First name: {props.firstName}</p>
+            <p>Last name: {props.lastName}</p>
+            <p>Gender: {props.gender}</p>
+            <p>Height: {props.height}</p>
+            <p>Birth: {props.Birth}</p>
+        </div>
+        
+   
+    </div>
+  )
+}
+
+export default IdCard
