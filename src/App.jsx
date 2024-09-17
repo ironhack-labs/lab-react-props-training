0ac10aa23f523de9b7b3efee034524fc2ad54f0a
Núria Soley <nuria.soley.93@gmail.com>
2024-09-17 16:27:05 +0200

    lab a la mitad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import IdCard from "./components/IdCard"
+import Greetings from './components/Greetings'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <h1> LAB | React Training</h1>
+      <div>
+        <IdCard
+            lastName='Doe'
+            firstName='John'
+            gender='male'
+            height={178}
+            birth={new Date("1992-07-14")}
+            picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+
+        <IdCard
+            lastName='Delores '
+            firstName='Obrien'
+            gender='female'
+            height={172}
+            birth={new Date("1988-05-11")}
+            picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,10 @@
+function Greetings(props) {
+  return (
+    <div>
+      <p>{lang.props}</p>
+      <p>{Children.props}</p>
+    </div>
+  )
+}
+
+export default Greetings
