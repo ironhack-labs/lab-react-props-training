64aa205a8d4dfebf595a750a04d137e128d16e6a
Núria Soley <nuria.soley.93@gmail.com>
2024-09-17 22:22:01 +0200

    done

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,8 +1,18 @@
 function Greetings(props) {
+  let greetings
+  if (props.lang === "de"){
+    greetings = "Hallo"
+  } else if (props.lang === "en"){
+    greetings = "Hello"
+  } else if (props.lang === "es"){
+    greetings = "Hola"
+  } else if (props.lang === "fr"){
+    greetings = "Bonjour"
+  }
+
   return (
-    <div>
-      <p>{lang.props}</p>
-      <p>{Children.props}</p>
+    <div id="greetings">
+      <p>{greetings} {props.children}</p>
     </div>
   )
 }
