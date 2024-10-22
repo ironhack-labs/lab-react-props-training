dd6e067b6f9675de99c5b15b4f4cfc04465d18b3
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-23 00:36:11 +0200

    iterations 1&2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import IdCard from "./components/IdCard";
+import Greetings from "./components/Greetings"; 
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1> LAB | React Training</h1>
     </div>
+    <IdCard />
+    <Greetings />
+    </>
+    
+    
   );
 }
 
