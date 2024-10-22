b61e60e9336b93837ea94826c69103aef0ac7536
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-22 23:17:52 +0200

    added language random number and colored cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Greetings from "./components/Greetings";
 import IdCard from "./components/IdCard";
-
+import Random from "./components/Random";
+import BoxColor from "./components/BoxColor";
 function App() {
   return (
     <div className="App">
@@ -22,7 +24,13 @@ function App() {
         birth={new Date("1988-05-11")}
         picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
-    </div>
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />    </div>
   );
 }
 
