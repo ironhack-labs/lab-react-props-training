97372dd1635f5b4f258240e0b379db5ef5a869a9
Sven Ole Spindler <s.spindler@gmx.de>
2024-11-08 16:15:15 +0100

    Iteration 1 done => implemented IdCard Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import IdCard from "./components/IdCard";
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <h1>LAB | React Training</h1>
+      <div>
+        <h2>IdCard</h2>
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Date("1992-07-14")}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+        <IdCard
+          lastName="Delores"
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+      <div>
+        <h2></h2>
+      </div>
     </div>
   );
 }
