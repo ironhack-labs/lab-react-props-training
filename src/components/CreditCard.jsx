91e22537a9f8178bac2577ca5d24545eb5b3bcc8
Mr Footwork <pandau.ting@gmail.com>
2024-11-12 09:53:04 +0100

    🎨 style credit cards

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,4 +1,6 @@
 import '../assets/css/CreditCard.css';
+import visaLogo from '../assets/images/visa.png';
+import masterCardLogo from '../assets/images/master-card.svg';
 
 function CreditCard({
 	type,
@@ -11,19 +13,23 @@ function CreditCard({
 	color,
 }) {
 	// prettier-ignore
-	const expirationDate = `${expirationMonth.toString().padStart(2,'0')}/${expirationYear}`;
+	const expirationDate = `${expirationMonth.toString().padStart(2,'0')}/${expirationYear.toString().slice(-2)}`;
 	const creditCardNumberSecure = `${number.slice(-4)}`;
 
+	const cardTypeSource = type === 'Visa' ? visaLogo : masterCardLogo;
+
 	return (
-		<div className='credit-card' style={{ backgroundColor: bgColor }}>
-			<div className='logo-container'></div>
+		<div className='credit-card' style={{ backgroundColor: bgColor, color }}>
+			<div className='card-type-container'>
+				<img src={cardTypeSource} alt='' />
+			</div>
 			<div className='credit-card-number-container'>
 				<span>⋅⋅⋅⋅ ⋅⋅⋅⋅ ⋅⋅⋅⋅ </span>
 				{creditCardNumberSecure}
 			</div>
 			<div className='credit-card-info-container'>
 				<div className='first-row'>
-					<p>{expirationDate}</p>
+					<p>Expires {expirationDate}</p>
 					<p>{bank}</p>
 				</div>
 				<div className='second-row'>
