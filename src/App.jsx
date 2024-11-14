27c6a1ba07601a98b57fdc703c0b3e9411573f1c
hurtaditoo <al415507@uji.es>
2024-11-14 20:10:38 +0100

    Iteration 1 | Component: IdCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import "./App.css";
+import IdCard from "./components/id-card/id-card";
 
 function App() {
   return (
-    <div className="App">
-      <h1> LAB | React Training</h1>
+    <div className="container py-5">
+      <IdCard
+        className="mb-2"
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
     </div>
   );
 }
---
+++ src/components/greetings/greetings.jsx
@@ -0,0 +1,21 @@
+
+// function Greetings({ className = '', lang = 'en', children }) {
+//     let greet = '';
+//     switch (lang) {
+//         case 'de':
+//             greet = 'Hallo';
+//             break;
+//         case 'en':
+//             greet = 'Hello';
+//             break;
+//         case 'es':
+//             greet = 'Hola';
+//             break;
+//         case 'fr':
+//             greet = 'Bonjour';
+//             break;
+        
+//     }
+// }
+
+// export default Greetings;
