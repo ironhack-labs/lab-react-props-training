987d52f5568968cf4618cfcafa6404b9879277e3
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-11-16 01:46:04 +0100

    Iteration 3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import IdCard from "./components/IdCard";
 import Greetings from "./components/Greetings";
+import Random from "./components/Random";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
       <Greetings lang="de">Ludwig</Greetings>
       <br></br>
       <Greetings lang="fr">François</Greetings>
+      <Random min={1} max={6}/>
+      <Random min={1} max={100}/>
     </div>
   );
 }
