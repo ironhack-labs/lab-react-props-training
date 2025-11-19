c2dc0197669e2fb404866df0293fa94a6a455040
Akriti sharma <aakritisharma41@gmail.com>
2025-11-19 19:57:35 +0100

    done

---
+++ src/components/DriverCard.jsx
@@ -0,0 +1,16 @@
+import Rating from "./Rating";
+
+function DriverCard({ name, rating, img, car }) {
+  return (
+    <div className="driver-card">
+      <img src={img} alt={name} />
+      <div className="driver-info">
+        <h2>{name}</h2>
+        <Rating>{rating}</Rating>
+        <p>{car.model} - {car.licensePlate}</p>
+      </div>
+    </div>
+  );
+}
+
+export default DriverCard;
