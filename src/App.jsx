81ecbd4da19a16f0dc5891f991c96fb086b351c9
Rafa <rafacruiz@gmail.com>
2025-12-02 23:47:14 +0100

    feat: done iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
 
-import IdCard from "./components/IdCard";
-import Greetings from "./components/Greetings";
-import Random from "./components/Random";
+import IdCard from "./components/id-card";
+import Greetings from "./components/greetings";
+import Random from "./components/random";
+import CreditCard from "./components/credit-card";
 
 function App() {
   return (
@@ -32,6 +33,39 @@ function App() {
 
       <Random min={1} max={6}/>
       <Random min={1} max={100}/>
+
+      <CreditCard
+        type="Visa"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white" 
+      />
+                
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
+          
+      <CreditCard
+        type="Visa"
+        number="0123456789016984"
+        expirationMonth={12}
+        expirationYear={2019}
+        bank="Name of the Bank"
+        owner="Firstname Lastname"
+        bgColor="#ddbb55"
+        color="white" 
+      />
 
     </div>
   );
